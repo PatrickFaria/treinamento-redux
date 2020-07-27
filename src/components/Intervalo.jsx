cfdbde70162e7e6f33b27eacef9020e48fa90e5b
Patrick Faria <patrickarfaria@gmail.com>
2020-07-27 06:24:41 -0300

    first commit - projeto finalizado

---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,52 @@
+import './Intervalo.css'
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Card from './Card'
+import { alterarNumeroMin } from '../store/actions/numerosActions'
+import { alterarNumeroMax } from '../store/actions/numerosActions'
+
+function Intervalo(props) {
+    const { min, max } = props
+    return (
+        <Card title="Intervalo de Números" red>
+            <div className="Intervalo">
+                <span>
+                    <strong>Mínimo:</strong>
+                    <input type="number" value={min}
+                    onChange={e => props.alterarMin(+e.target.value)} />
+                </span>
+                <span>
+                    <strong>Máximo:</strong>
+                    <input type="number" value={max}
+                    onChange={e => props.alterarMax(+e.target.value)} />
+                </span>
+            </div>
+        </Card>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        alterarMin(novoNumero) {
+            const action = alterarNumeroMin(novoNumero)
+            dispatch(action)
+        },
+        alterarMax(novoNumero){
+            const action = alterarNumeroMax(novoNumero)
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Intervalo)
---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Card from './Card'
+
+function Sorteio(props) {
+    const { min, max } = props
+    const random = parseInt(Math.random() * (max - min)) + min;
+    return (
+        <Card title="Sorteio dos Números" purple>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{random}</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Sorteio)
